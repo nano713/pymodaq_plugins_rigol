d81d05e15e16c99e1a0b0bf31183488a0b11df89
adeshazer123 <adeshazer@wisc.edu>
2025-02-01 01:39:38 -0600

    Add if statements and update commands

--- src/pymodaq_plugins_template/hardware/Dsa815_VISADriver.py
+++ src/pymodaq_plugins_template/hardware/Dsa815_VISADriver.py
@@ -11,9 +11,20 @@ class DSA815:
     
     def set_start_freq(self, start_freq): 
         """Set the start frequency of the spectrum analyzer"""
-        self._spectrum.write(":SENS:FREQ:STAR %e HZ")
-        logger.info(f"Set start frequency to {start_freq} Hz")
+        if start_freq > 0 and start_freq < 7.5e9:
+            self._spectrum.write(f":SENS:FREQ:STAR {start_freq} HZ")
+            logger.info(f"Set start frequency to {start_freq} Hz")
     
     def get_start_freq(self): 
         """Get the start frequency of the spectrum analyzer"""
         return self._spectrum.query(":SENSe:FREQuency:STARt?")
+    
+    def set_center_freq(self, center_freq): 
+        """Set the center frequency of the spectrum analyzer"""
+        if center_freq > 0 and center_freq < 7.5e9:
+            self._spectrum.write(f":SENSe:FREQuency:CENTer {center_freq} HZ")
+            logger.info(f"Set center frequency to {center_freq} Hz")
+    
+    def get_center_freq(self): 
+        """Get the center frequency of the spectrum analyzer"""
+        return self._spectrum.query(":SENSe:FREQuency:CENTer?")
