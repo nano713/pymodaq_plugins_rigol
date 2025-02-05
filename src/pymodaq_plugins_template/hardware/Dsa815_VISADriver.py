6c8cf456d1851e15c90210153efb1e7fd58765f7
nano713 <50872819+nano713@users.noreply.github.com>
2025-02-05 13:04:04 +0900

    Refactor hardware. Needs to add grab_data.
    Co-authored-by: Amelie Francoise Deshazer <adeshazer123@users.noreply.github.com>

--- src/pymodaq_plugins_template/hardware/Dsa815_VISADriver.py
+++ src/pymodaq_plugins_template/hardware/Dsa815_VISADriver.py
@@ -5,43 +5,48 @@ logger = logging.getLogger(__name__)
 
 class DSA815: 
     def __init__(self):
-        rm = pyvisa.ResourceManager()
-        self._spectrum = rm.open_resource() # Open the first available resource and fix this
+        self.rm = pyvisa.ResourceManager()
+        self.read_termination = '/n'
+        self._spectrum = None # Open the first available resource and fix this
         
-    def connect(self, address): 
+    def connect(self, address):
         """Connect to the spectrum analyzer"""
-        self._spectrum.open_resource(address)
+        self._spectrum = self.rm.open_resource(address)
+        self._spectrum.read_termination = '\n'
         logger.info(f"Connected to {address}")
     
     def set_start_freq(self, start_freq): 
         """Set the start frequency of the spectrum analyzer"""
         if start_freq > 0 and start_freq < 7.5e9:
-            self._spectrum.write(f":SENS:FREQ:STAR {start_freq} HZ")
+            self._spectrum.write(f":SENS:FREQ:STAR {start_freq}")
             logger.info(f"Set start frequency to {start_freq} Hz")
     
     def get_start_freq(self): 
         """Get the start frequency of the spectrum analyzer"""
-        return self._spectrum.query(":SENSe:FREQuency:STARt?")
+        frequency = self._spectrum.query(":SENSe:FREQuency:STARt?")
+        return int(frequency)
     
     def set_center_freq(self, center_freq): 
         """Set the center frequency of the spectrum analyzer"""
         if center_freq > 0 and center_freq < 7.5e9:
-            self._spectrum.write(f":SENSe:FREQuency:CENTer {center_freq} HZ")
+            self._spectrum.write(f":SENSe:FREQuency:CENTer {center_freq}")
             logger.info(f"Set center frequency to {center_freq} Hz")
     
     def get_center_freq(self): 
         """Get the center frequency of the spectrum analyzer"""
-        return self._spectrum.query(":SENSe:FREQuency:CENTer?")
+        frequency = self._spectrum.query(":SENSe:FREQuency:CENTer?")
+        return int(frequency)
     
     def set_stop_freq(self, stop_freq): 
         """Set the stop frequency of the spectrum analyzer"""
         if stop_freq > 0 and stop_freq < 7.5e9:
-            self._spectrum.write(f":SENSe:FREQuency:STOP {stop_freq} HZ")
+            self._spectrum.write(f":SENSe:FREQuency:STOP {stop_freq}")
             logger.info(f"Set stop frequency to {stop_freq} Hz")
     
     def get_stop_freq(self): 
         """Get the stop frequency of the spectrum analyzer"""
-        return self._spectrum.query(":SENSe:FREQuency:STOP?")
+        frequency = self._spectrum.query(":SENSe:FREQuency:STOP?")
+        return int(frequency)
     
     def set_freq_points(self, freq_points): 
         """Set the number of frequency points of the spectrum analyzer"""
@@ -51,10 +56,13 @@ class DSA815:
     
     def get_freq_points(self): 
         """Get the number of frequency points of the spectrum analyzer"""
-        return self._spectrum.query(":SENSe:SWEep:POINts?")
+        points = self._spectrum.query(":SENSe:SWEep:POINts?") # FIX? DK - crashed
+        return int(points)
     
-    def set_sweep_time(self, sweep_time): 
-        """Set the sweep time of the spectrum analyzer"""
+    def set_sweep_time(self, sweep_time):  
+        """Set the sweep time of the spectrum analyzer
+        sweep_time: float (s)
+        """
         if sweep_time > 2e-5 and sweep_time < 7500:
             self._spectrum.write(f":SENSe:SWEep:TIME {sweep_time}")
             logger.info(f"Set sweep time to {sweep_time}")
@@ -75,3 +83,5 @@ class DSA815:
     def get_continuous_sweep(self): 
         """Get the continuous sweep of the spectrum analyzer"""
         return self._spectrum.query(":INITiate:CONTinuous?")
+    
+    
